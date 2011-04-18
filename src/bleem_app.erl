750998cce641b0ef6d06304fd2390a0efbb290bc
Uwe Dauernheim <uwe@klarna.com>
2011-04-18 21:00:08 +0200

    Initial rebar file structure

---
+++ src/bleem_app.erl
@@ -0,0 +1,16 @@
+-module(bleem_app).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+
+%% ===================================================================
+%% Application callbacks
+%% ===================================================================
+
+start(_StartType, _StartArgs) ->
+    bleem_sup:start_link().
+
+stop(_State) ->
+    ok.
