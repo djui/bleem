02b3faf75ecb89cc95381533646bf21f0e96cff9
Uwe Dauernheim <uwe@klarna.com>
2011-04-18 22:16:42 +0200

    Start to add tests

---
+++ test/gen_bleem_SUITE.erl
@@ -0,0 +1,12 @@
+%%% @doc gen_bleem test suite.
+%%% @author Uwe Dauernheim <uwe@dauernheim.net>
+-module(gen_bleem_SUITE).
+
+-author("Uwe Dauernheim <uwe@dauernheim.net>").
+
+-behaviour(gen_bleem).
+
+-export([ state/0
+        , bleem/1
+        ]).
+
